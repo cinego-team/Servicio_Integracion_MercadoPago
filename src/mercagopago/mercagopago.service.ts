8d3e5d1da533bff9d9de23d39d3cb54a4b8b4059
Ramiro Lynch <ramirolynch@hotmail.com>
2025-09-30 18:29:36 -0300

    3ero jeje

--- src/mercagopago/mercagopago.service.ts
+++ src/mercagopago/mercagopago.service.ts
@@ -3,19 +3,19 @@ import { MercadoPagoConfig, Preference } from 'mercadopago';
 
 @Injectable()
 export class MercagopagoService {
-    const client = new MercadoPagoConfig({
-  accessToken: process.env.MP_ACCESS_TOKEN || '',
-});
-    const preference = new Preference(client);
-    preference.create({
-    body: {
-        items: [
-        {
-            title: 'Mi producto',
-            quantity: 1,
-            unit_price: 2000
-        }
-        ],
-    }
-    })
+  //     const client = new MercadoPagoConfig({
+  //   accessToken: process.env.MP_ACCESS_TOKEN || '',
+  // });
+  //     const preference = new Preference(client);
+  //     preference.create({
+  //     body: {
+  //         items: [
+  //         {
+  //             title: 'Mi producto',
+  //             quantity: 1,
+  //             unit_price: 2000
+  //         }
+  //         ],
+  //     }
+  //     })
 }
