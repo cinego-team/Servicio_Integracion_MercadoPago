8bdd274de41e5652914549e66662d591adf2f6cf
Ramiro Lynch <ramirolynch@hotmail.com>
2026-02-06 19:51:42 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: [
+            'https://api-gateway-one-rose.vercel.app',
+            'https://servicio-usuarios.vercel.app',
+            'https://microservicio-de-peliculas.vercel.app',
+            'https://servicio-promociones.vercel.app',
+            'https://servicio-ventas-seven.vercel.app',
+            'https://servicio-funciones-y-salas.vercel.app',
+            'https://servicioenvioemails.vercel.app'
+        ],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true
+    });
     await app.listen(process.env.PORT ?? 3007);
 }
 bootstrap();
