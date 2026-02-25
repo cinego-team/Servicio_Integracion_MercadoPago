ad9949bdbbb6468c508265750e81dd06eb951c8a
Ramiro Lynch <ramirolynch@hotmail.com>
2026-02-24 23:44:48 -0300

    ultimando detalles

--- src/cobros/cobros.service.ts
+++ src/cobros/cobros.service.ts
@@ -12,16 +12,16 @@ export class CobrosService {
             items: [
                 {
                     id: '1',
-                    title: String(data.titulo),
+                    title: String(data.descripcion),
                     quantity: 1,
                     unit_price: Number(data.monto),
                     currency_id: 'ARS',
                 },
             ],
             back_urls: {
-                success: `https://talk-tuner-string-simulation.trycloudflare.com/pantalla-exito`, //link solo valido para pc rama
-                failure: `https://talk-tuner-string-simulation.trycloudflare.com/pantalla-fracaso`,
-                pending: `https://talk-tuner-string-simulation.trycloudflare.com/pantalla-pendiente`,
+                success: `https://increasingly-thomas-subsidiaries-benefit.trycloudflare.com/pantalla-exito`, //link solo valido para pc rama
+                failure: `https://increasingly-thomas-subsidiaries-benefit.trycloudflare.com/pantalla-fracaso`,
+                pending: `https://increasingly-thomas-subsidiaries-benefit.trycloudflare.com/pantalla-pendiente`,
             },
             auto_return: 'approved',
             notification_url: `https://susanne-tressured-fidel.ngrok-free.dev/mercadopago/webhook`, //link solo valido para pc rama
